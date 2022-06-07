04e31f570aa608b306d08ff0e7fdb50afa3accdc
Nate Ferrero <nate@tagme.in>
2022-06-06 21:14:48 -0400

    resize bug

--- public/fx/stars.js
+++ public/fx/stars.js
@@ -1,7 +1,6 @@
 function space(canvas) {
  const spaceCanvas = canvas ?? document.createElement('canvas')
  const { innerWidth: width, innerHeight: height } = window
- Object.assign(spaceCanvas, { width, height })
  Object.assign(spaceCanvas.style, {
   backgroundColor: 'transparent',
   pointerEvents: 'none',
@@ -14,21 +13,21 @@ function space(canvas) {
   opacity: 0.5
  })
  document.body.appendChild(spaceCanvas)
- return spaceCanvas.getContext('2d')
+ return { canvas: spaceCanvas, context: spaceCanvas.getContext('2d') }
 }
 
 window.spaceLayer = space()
 
 function stars() {
-
  const { innerWidth: width, innerHeight: height } = window
- window.spaceLayer.fillStyle = '#f9f9e006'
- window.spaceLayer.clearRect(0, 0, width, height)
+ Object.assign(window.spaceLayer.canvas, { width, height })
+ window.spaceLayer.context.fillStyle = '#f9f9e008'
+ window.spaceLayer.context.clearRect(0, 0, width, height)
  for (const id in new Array(1e5).fill()) {
   const size = (( id * id ) % 4 ) + 1
   const x = (12309814230987 * id) % width - size
   const y = 12309814230987 * id % height - size
-  window.spaceLayer.fillRect(x, y, size * 2, size * 2)
+  window.spaceLayer.context.fillRect(x, y, size * 2, size * 2)
  }
 }
 
