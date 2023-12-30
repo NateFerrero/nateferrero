7901485b6388d36b658b1c75ee9a1fdbe6201c6d
Nate Ferrero <nate@tagme.in>
2023-12-30 00:18:17 -0500

    Add ikigai

--- public/engine.js
+++ public/engine.js
@@ -66,9 +66,7 @@ const content = document.createElement('div')
 content.classList.add('content')
 container.appendChild(content)
 
-for (const section of 'Coaching Contact Software Writing'.split(
- ' '
-)) {
+for (const section of Object.keys(globalThis.MAIN).sort()) {
  const link = document.createElement('a')
  link.setAttribute('href', `/#/${section.toLowerCase()}`)
  link.textContent = section
@@ -94,64 +92,16 @@ function route() {
  content.innerHTML = ''
  switch (location.hash) {
   case '#/coaching':
-   content.innerHTML = `<p>I offer live coaching on a variety of subjects. My coaching fee is <b>USD$80<sup>.00</sup>/hr</b>.</p>
-    <p>You may also purchase blocks of 10 hours in advance for a <b>USD$600<sup>.00</sup></b> flat fee.</p>
-    <p>I meet with clients virtually using Google Meet, Zoom, or Jump Chat. Sessions are scheduled Monday through Friday, 1pm to 9pm Eastern Time.</p>
-    <p>For those in Michigan, United States, I also offer in-person consultations in Ann Arbor.</p>
-    <p>Sessions are usually 1 or 2 hours long, and as a client, you can ask me questions by text message and expect a reply within 24 hours.</p>
-    <h3>Coaching subjects</h3>
-    <ul>
-     <li>AI-assisted discovery of life purpose &lsquo;Ikigai&rsquo; &mdash; identify a career that will motivate and inspire you to succeed</li>
-     <li>JavaScript, TypeScript, and web development fundamentals; fixing software bugs in real time; learning to code by solving coding challenges together</li>
-     <li>Sensitivity and spiritual growth; ethical living</li>
-     <li>Social skills and personal development</li>
-     <li>Identifying and transcending life obstacles</li>
-     </ul>
-    <p><a href="/#/contact">Contact to schedule your first session &rarr;</p>`
+   content.innerHTML = globalThis.MAIN.Coaching
    break
   case '#/contact':
-   content.innerHTML = `
-   <p>For questions related to <a target="_blank" href="https://tagme.in">https://tagme.in</a>, please email <a href="mailto:nate@tagme.in">nate<span>@</span>tagme.in</a></p>
-   <p>For all other inquiries, email <a href="mailto:hello@nateferrero.com">hello<span>@</span>nateferrero.com</a></p>
-   <p>Text or phone call at <b>+1 (323) 283-9179</b></p>
-   <p>When writing, please introduce yourself and state your request in the first message</p>
-    <h3>Social media</h3>
-    <ul>
-     <li><a target="_blank" href="https://www.codementor.io/@nateferrero">Codementor</a></li>
-     <li><a target="_blank" href="https://www.facebook.com/nateferrero/">Facebook</a></li>
-     <li><a target="_blank" href="https://github.com/NateFerrero">GitHub</a></li>
-     <li><a target="_blank" href="https://ailaai.app/nate">Hi Town</a></li>
-     <li><a target="_blank" href="https://www.instagram.com/nateferrero/">Instagram</a></li>
-     <li><a target="_blank" href="https://www.linkedin.com/in/nateferrero/">LinkedIn</a></li>
-     <li><a target="_blank" href="https://stackoverflow.com/users/1008429/nate-ferrero">Stack Overflow</a></li>
-     <li><a target="_blank" href="https://twitter.com/NateFerrero">Twitter / X</a></li>
-     <li><a target="_blank" href="https://www.youtube.com/NateFerrero">YouTube</a></li>
-    </ul>
-    `
+   content.innerHTML = globalThis.MAIN.Contact
    break
   case '#/software':
-   content.innerHTML = `
-    <h3>Mission statement</h3>
-    <p>To possess deep understanding of <a target="_blank" href="https://kk.org/thetechnium/">The Technium</a>, and identify improvements to be made that cause positive impact on an individual, society, or the world.</p>
-    <h3>Why is this my mission?</h3>
-    <p>Because as a mammal, I have a limited lifespan. Patches of code I contribute to the Technium will outlive me.</p>
-    <h3>Software projects</h3>
-    <ul>
-     <li><a target="_blank" href="https://civil-compute.com/">Civil Compute</a> - a software development and hosting platform</li>
-     <li><a target="_blank" href="https://civil-lang.org/">Civil Language</a> - a programming language and development environment optimized for conciseness, implemented in JavaScript</li>
-     <li><a target="_blank" href="https://crown-lang.org/">Crown</a> - a programming metalanguage optimized for ease of use and ability to communicate verbally, implemented in JavaScript</li>
-     <li><a target="_blank" href="https://journal.network/">Journal Network</a> - a slow text-only social network</li>
-     <li><a target="_blank" href="https://listmanager.dev/">List Manager</a> - an information organizer</li>
-     <li><a target="_blank" href="https://tagme.in/">TagMeIn</a> - a social network for software developers</li>
-     <li><a target="_blank" href="https://realisticabundance.com/">Realistic Abundance</a> - a conceptual framework for invoking global prosperity</li>
-    </ul>
-    <p>See <a href="https://github.com/tagmein/">TagMeIn on GitHub</a> for more projects.</p>
-     `
+   content.innerHTML = globalThis.MAIN.Software
    break
   case '#/writing':
-   content.innerHTML = `
-    <h3>🚧 Under construction 🚧</h3>
-   `
+   content.innerHTML = globalThis.MAIN.Writing
    break
  }
  backToHome.classList[
---
+++ public/main.js
@@ -0,0 +1,101 @@
+globalThis.MAIN = {
+ Coaching: `
+  <h3>Learn with Nate</h3>
+  <p>I offer live coaching on a variety of subjects. My coaching fee is <b>USD$80<sup>.00</sup>/hr</b>.</p>
+  <p>You may also purchase blocks of 10 hours in advance for a <b>USD$600<sup>.00</sup></b> flat fee.</p>
+  <p>I meet with clients virtually using video conferencing software such as Zoom, Google Meet, or Jump Chat.
+   Sessions are scheduled Monday through Friday, 1pm to 9pm Eastern Time.</p>
+  <p>For those in Michigan, United States, I also offer in-person consultations in Ann Arbor.</p>
+  <p>Sessions are usually 1 or 2 hours long, and as a client, you can ask me questions by text message and expect a reply within 24 hours.</p>
+  <h3>Coaching subjects</h3>
+  <ul>
+   <li>Web development fundamentals: JavaScript, TypeScript, CSS, and HTML; fixing software bugs in real time; learning to code by solving coding challenges together</li>
+   <li>Sensitivity and spiritual growth; ethical living</li>
+   <li>Social skills and personal development</li>
+   <li>Identifying and transcending life obstacles</li>
+   <li>AI-assisted discovery of life purpose &lsquo;Ikigai&rsquo; &mdash; identify a career that will motivate and inspire you to succeed</li>
+  </ul>
+  <div style="text-align: center;">
+   <div style="display: inline-block; aspect-ratio: 1; max-width: 100%; position: relative; width: 500px; height: 500px; margin: -50px;">
+    <div style="border: 2px solid white; position: absolute; top: 40%; left: 40%; width: 240px; height: 240px; margin-top: -120px; margin-left: -120px; border-radius: 50%; background: #ffff00a0; mix-blend-mode: screen;"></div>
+    <div style="border: 2px solid white; position: absolute; top: 40%; right:40%; width: 240px; height: 240px; margin-top: -120px; margin-right: -120px; border-radius: 50%; background: #ff8000a0; mix-blend-mode: screen;"></div>
+    <div style="border: 2px solid white; position: absolute; bottom: 40%; left: 40%; width: 240px; height: 240px; margin-bottom: -120px; margin-left: -120px; border-radius: 50%; background: #0000ffa0; mix-blend-mode: screen;"></div>
+    <div style="border: 2px solid white; position: absolute; bottom: 40%; right:40%; width: 240px; height: 240px; margin-bottom: -120px; margin-right: -120px; border-radius: 50%; background: #00ff00a0; mix-blend-mode: screen;"></div>
+    <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: #303030;">Ikigai</div>
+    <div style="position: absolute; top: 50%; left: 22.5%; transform: translate(0, -50%); color: #303030;">Mission</div>
+    <div style="position: absolute; top: 25%; left: 50%; transform: translate(-50%, 0); color: #303030;">Passion</div>
+    <div style="position: absolute; top: 50%; right: 21.5%; transform: translate(0, -50%); color: #303030;">Vocation</div>
+    <div style="position: absolute; bottom: 25%; left: 50%; transform: translate(-50%, -50%); color: #303030;">Profession</div>
+
+    <div style="text-shadow: 0 0 2px #000, 0 0 2px #000, 0 0 2px #000; text-align: center; font-weight: bold; font-size: 80%; position: absolute; top: 29%; left: 24%; transform: translate(0, -50%); color: #ffffff;">I love</div>
+    <div style="text-shadow: 0 0 2px #000, 0 0 2px #000, 0 0 2px #000; text-align: center; font-weight: bold; font-size: 80%; position: absolute; top: 23%; left: 70%; width: 40px; transform: translate(-50%, 0); color: #ffffff;">I am needed</div>
+    <div style="text-shadow: 0 0 2px #000, 0 0 2px #000, 0 0 2px #000; text-align: center; font-weight: bold; font-size: 80%; position: absolute; bottom: 22%; right: 21%; width: 80px; transform: translate(0, -50%); color: #ffffff;">I am competent</div>
+    <div style="text-shadow: 0 0 2px #000, 0 0 2px #000, 0 0 2px #000; text-align: center; font-weight: bold; font-size: 80%; position: absolute; bottom: 22%; left: 28%; width: 60px; transform: translate(-50%, -50%); color: #ffffff;">I am valued</div>
+   </div>
+  </div>
+  <h3>About Nate</h3>
+  <p>Nate has been developing his teaching skills for 19 years, starting as a Math Lab tutor at Grand Rapids Community College, an Academic Support tutor at Grand Valley State University where he individually tutored over 55 college students in math, physics, and chemistry until earning his B.S. in Physics in 2007.</p>
+  <p>After graduating, Nate pursued a career in Software Development where he has mentored numerous developers at various skill levels in a range of industries, from Finance and Marketing to Gaming and SaaS. Nate continues to mentor aspiring software engineers to this day.</p>
+  <p>He also has experience working with children and high school students. He was a volunteer tutor at Central High School in Grand Rapids, MI for one summer in the after-school support lab.</p>
+  <h3>Testimonials</h3>
+  <blockquote>
+   <p>⭐⭐⭐⭐⭐</p>
+   <p>awesome session with Nate. He helps me resolve a tricky issue that took me days but with him, just 50 minutes. thank you so much, Nate. Highly recommend.</p>
+   <p><em>&mdash; Tu T., software coaching client</em></p>
+  </blockquote>
+  <blockquote>
+   <p>⭐⭐⭐⭐⭐</p>
+   <p>Could not ask for a better mentor. Grateful to have found Nate. Thank you.</p>
+   <p><em>&mdash; Silvia F., software coaching client</em></p>
+  </blockquote>
+  <blockquote>
+   <p>⭐⭐⭐⭐⭐</p>
+   <p>I have been using Nate for a while now and if their was an option to give him a sixth star I would. His style of working with you is always to help you understand the solution and not just fix the problem. He always looks for the best solution to the problem not just the fastest solution. I have worked with many different mentors on codementor and by far Nate is a top tier mentor.</p>
+   <p><em>&mdash; Luis B., software coaching client</em></p>
+  </blockquote>
+  <blockquote>
+   <p>⭐⭐⭐⭐⭐</p>
+   <p>Great mentor! Really knowledgable and was able to learn a lot in one session!</p>
+   <p><em>&mdash; H. M., software coaching client</em></p>
+  </blockquote>
+  <p><a href="/#/contact">Contact to schedule your first session &rarr;</p>
+ `,
+ Contact: `
+  <p>For questions related to <a target="_blank" href="https://tagme.in">https://tagme.in</a>, please email <a href="mailto:nate@tagme.in">nate<span>@</span>tagme.in</a></p>
+  <p>For all other inquiries, email <a href="mailto:hello@nateferrero.com">hello<span>@</span>nateferrero.com</a></p>
+  <p>Text or phone call at <b>+1 (323) 283-9179</b></p>
+  <p>When writing, please introduce yourself and state your request in the first message</p>
+   <h3>Social media</h3>
+   <ul>
+    <li><a target="_blank" href="https://www.codementor.io/@nateferrero">Codementor</a></li>
+    <li><a target="_blank" href="https://www.facebook.com/nateferrero/">Facebook</a></li>
+    <li><a target="_blank" href="https://github.com/NateFerrero">GitHub</a></li>
+    <li><a target="_blank" href="https://ailaai.app/nate">Hi Town</a></li>
+    <li><a target="_blank" href="https://www.instagram.com/nateferrero/">Instagram</a></li>
+    <li><a target="_blank" href="https://www.linkedin.com/in/nateferrero/">LinkedIn</a></li>
+    <li><a target="_blank" href="https://stackoverflow.com/users/1008429/nate-ferrero">Stack Overflow</a></li>
+    <li><a target="_blank" href="https://twitter.com/NateFerrero">Twitter / X</a></li>
+    <li><a target="_blank" href="https://www.youtube.com/NateFerrero">YouTube</a></li>
+   </ul>
+  `,
+ Software: `
+   <h3>Mission statement</h3>
+   <p>To possess deep understanding of <a target="_blank" href="https://kk.org/thetechnium/">The Technium</a>, and identify improvements to be made that cause positive impact on an individual, society, or the world.</p>
+   <h3>Why is this my mission?</h3>
+   <p>Because as a mammal, I have a limited lifespan. Patches of code I contribute to the Technium will outlive me.</p>
+   <h3>Software projects</h3>
+   <ul>
+    <li><a target="_blank" href="https://civil-compute.com/">Civil Compute</a> - a software development and hosting platform</li>
+    <li><a target="_blank" href="https://civil-lang.org/">Civil Language</a> - a programming language and development environment optimized for conciseness, implemented in JavaScript</li>
+    <li><a target="_blank" href="https://crown-lang.org/">Crown</a> - a programming metalanguage optimized for ease of use and ability to communicate verbally, implemented in JavaScript</li>
+    <li><a target="_blank" href="https://journal.network/">Journal Network</a> - a slow text-only social network</li>
+    <li><a target="_blank" href="https://listmanager.dev/">List Manager</a> - an information organizer</li>
+    <li><a target="_blank" href="https://tagme.in/">TagMeIn</a> - a social network for software developers</li>
+    <li><a target="_blank" href="https://realisticabundance.com/">Realistic Abundance</a> - a conceptual framework for invoking global prosperity</li>
+   </ul>
+   <p>See <a href="https://github.com/tagmein/">TagMeIn on GitHub</a> for more projects.</p>
+  `,
+ Writing: `
+  <h3>🚧 Under construction 🚧</h3>
+ `,
+}
