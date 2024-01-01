95cf5d6691b8a939ce1d4cd651d29eeba796db11
Nate Ferrero <nate@tagme.in>
2024-01-01 10:24:49 -0500

    proverbs in 3s

---
+++ public/content/1704122230121.mjs
@@ -0,0 +1,17 @@
+export const published = true
+export const tags = [
+ 'Life',
+ 'The Bible',
+ 'Proverbs',
+ 'Wisdom',
+]
+export const time = 1704122578490
+export const title = 'Proverbs in Threes, #1'
+export const snippet = 'Proverbs 3:31; 17:3; 18:2.'
+export const content = `
+Envy you not the oppressor, and choose none of his ways. <cite>3:31</cite>
+
+The fining pot is for silver, and the furnace for gold: but the L<small>ORD</small> tries the hearts. <cite>17:3</cite>
+
+A fool has no delight in understanding, but that his heart may discover itself. <cite>18:2</cite>
+`
