bdc66a48d20f66b9c4840afedc05799588024283
Nate Ferrero <nate@tagme.in>
2024-03-05 14:39:31 -0500

    no bad people tags

--- public/content/1704122844518.mjs
+++ public/content/1704122844518.mjs
@@ -1,5 +1,9 @@
 export const published = true
-export const tags = ['Spirituality', 'True stories']
+export const tags = [
+ 'Burning Man',
+ 'Spirituality',
+ 'True stories',
+]
 export const time = 1709667382760
 export const title = 'There are no bad people'
 export const snippet =
