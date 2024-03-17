357c8f2f5099beffc321e8ba210dac6d9c619f4d
Nate Ferrero <nate@tagme.in>
2024-03-17 16:57:10 -0400

    Update engine.js
    Add Tools of the Technium

--- public/engine.js
+++ public/engine.js
@@ -95,8 +95,14 @@ function route() {
     <p>➡️ <a href="https://tagme.in/#/WorldChangingIdeas" target="_blank">World Changing Ideas</a></p>
 
     <h2>Subscribe to my writing</h2>
+    <details>
+     <summary>Technology and software</summary>
+     <iframe src="https://toolsofthetechnium.substack.com/embed" width="100%" height="480" style="border:1px solid #EEE; background:white;" frameborder="0" scrolling="no"></iframe>
+    </details>
+    <details>
+     <summary>Spirituality and religion</summary>
     <iframe src="https://landingatzion.substack.com/embed" width="100%" height="480" style="border:1px solid #EEE; background:white;" frameborder="0" scrolling="no"></iframe>
-
+    </details>
     <p class="info-text">Website built with assistance from with https://claude.ai and https://chat.openai.com</p>
      `
     break
